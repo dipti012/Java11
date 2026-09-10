144ee5d777da5ad8c3d42dcb5475a7516dfd1942
dipti012 <itpid12@gmail.com>
2026-09-10 02:36:48 -0700

    add file

--- Constructor.java
+++ Constructor.java
@@ -1,11 +1,33 @@
 // Create a book class with the following attributs:
-// book id
+// bookId
 // titel
 // author
 //Create a paramitalize constructor to initialize these value.
 // Create a methid displayInformation() to display the book details.
 //ln main(),create two book object using the constructor and display their information.
 class Book {
-    int 
+    int bookId;
+    String title;
+    String author;
+    Book(int bookId, String title,String author ){
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+    }
+        public void displayInformation(){
+            System.out.println("Book ID :" + bookId);
+            System.out.println("Title :" + title);
+            System.out.println("Author :" + author);
 
+
+        }
+    }
+
+
+public class Constructor{
+    public static void main(String[] args){
+        Book b1 = new Book(2 , "muna madan" , "hari");
+        b1.displayInformation();
+       
+    }
 }
