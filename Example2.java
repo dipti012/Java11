4b368b4600ab7fdab0cba1e9f744e1919678ed8e
dipti012 <itpid12@gmail.com>
2026-09-16 02:38:10 -0700

    new file

---
+++ Example2.java
@@ -0,0 +1,33 @@
+//Create a Student class with:
+
+//id
+//name
+//age
+//Create the following overloaded constructors:
+
+//Constructor with no arguments
+//Constructor with id and name
+//Constructor with id, name, and age
+class Student{
+    int id;
+    String name;
+    int age;
+    Student(int i ,String n){
+    id = i;
+    name = n;
+    }
+    Student(int i, String n,int a){
+        id = i;
+        name = n;
+        age = a;
+
+    }
+
+
+}
+void display(){
+    System.out.println("Id :" + i);
+    System.out.println("Name:" + n);
+    System.out.println("Age:" + a);
+
+}
